0dbf30c2510f248f1a866ab45749c3137eadd56b
matheus55391 <matheus.manulo55@gmail.com>
2024-09-08 12:24:00 -0300

    fix/delete test

--- src/__tests__/utils/iso-string-to-date.test.ts
+++
@@ -1,9 +0,0 @@
-export default function isoStringToDate(isoString: string): Date {
-    const date = new Date(isoString);
-  
-    if (isNaN(date.getTime())) {
-      throw new Error("Data inválida.");
-    }
-  
-    return date;
-  }
